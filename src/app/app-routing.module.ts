56e5baa652081913429b90b04c7ba0b1461239bd
Alan <a_saudabekov@kbtu.kz>
2023-10-15 20:36:23 +0600

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NotificationsComponent } from './notifications/notifications.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'activity', component: NotificationsComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { SearchComponent } from './search/search.component';
+import { NewBranchComponent } from './new-branch/new-branch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    ProfileComponent,
+    NotificationsComponent,
+    SearchComponent,
+    NewBranchComponent
   ],
   imports: [
     BrowserModule,

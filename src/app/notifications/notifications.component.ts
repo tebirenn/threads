66ff33ae3165ca0cb70e3b5bdc65afcb3d31faae
Adeem <75906616+adeemgoogle@users.noreply.github.com>
2023-10-15 22:21:25 +0600

    'activity'

--- src/app/notifications/notifications.component.ts
+++ src/app/notifications/notifications.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
-  selector: 'app-notifications',
+  selector: 'app-search',
   templateUrl: './notifications.component.html',
   styleUrls: ['./notifications.component.css']
 })
 export class NotificationsComponent {
+  accounts: any[] = [];
 
+  constructor(private dataService: DataService) {
+    const data = this.dataService.getData();
+    this.accounts = data.accounts;
+    console.log(this.accounts);
+  }
 }
